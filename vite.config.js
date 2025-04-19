82b1ebae0f7074436a7573bfd3f49c99fdf15819
Joyyojpeed <jds472016@gmail.com>
2025-04-20 04:40:57 +0530

    changes for local and vercel deployment together

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          env: process.env.NODE_ENV
+        }
+      }
+    })
+  ],
   base: '/',
   build: {
     manifest: true,
     rollupOptions: {
-      input: {
-        main: './index.html'
-      },
+      input: './index.html',
       output: {
         entryFileNames: 'assets/[name].[hash].js',
         assetFileNames: 'assets/[name].[hash].[ext]'
